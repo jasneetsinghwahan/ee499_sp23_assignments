614da7643f949c2b1e2ebf0151a11322353d5948
Jasneet Singh <jasneetsinghwahan@gmail.com>
2023-03-16 10:40:52 -0700

    feat(couple-of-jupyter-notebooks-trying-out-concepts-in-small-codes): these notebooks related to my experiments with the codes

--- ee499_sp23_assignments/installpackagesscript.sh
+++ ee499_sp23_assignments/installpackagesscript.sh
@@ -1,4 +1,20 @@
 #!/usr/bin/bash
 
-python ee499.py 
-python ee499_1.py 
+conda install numpy
+conda install pandas
+conda install seaborn
+conda install matplotlib
+conda install scikit-learn
+python -m pip install jupyter notebook -U
+conda install -n ML ipykernel --update-deps --force-reinstall
+mkdir generatedData
+conda install openpyxl
+conda install -c conda-forge xlsxwriter
+conda install -c anaconda h5py
+conda install -c conda-forge autograd
+conda install -c pytorch pytorch
+pip install torchvision
+pip install opencv-python
+pip install imutils
+pip install chardet
+pip install charset-normalizer
