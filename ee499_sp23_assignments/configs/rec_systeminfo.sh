7b1fdef9a29daf1fcf14b9dea302ed2b51202201
jasneetsinghwahan <jasneetsinghwahan@gmail.com>
2023-05-05 17:45:30 -0700

    some more system info configurations saved

--- ee499_sp23_assignments/configs/rec_systeminfo.sh
+++ ee499_sp23_assignments/configs/rec_systeminfo.sh
@@ -15,6 +15,12 @@ bison_version=$(bison --version | head -n 1)
 llvm_version=$(llvm-config --version)
 cmake_version=$(cmake --version | head -n 1)
 gcc_version=$(gcc --version | head -n 1)
+ubuntu_version=$(cat /etc/lsb-release)
+linker_version=$(ld -v)
+make_version=$(make --version)
+util_linux_version=$(fdformat --version)
+module_init_tools=$(depmod -V)
+packinst_list=$(dpkg -l)
 
 # Print the system info to the console
 echo "pwd: $PWD"
@@ -24,6 +30,12 @@ echo "bison version: $bison_version"
 echo "LLVM version: $llvm_version"
 echo "cMake version: $cmake_version"
 echo "gcc version: $gcc_version"
+echo "linker version: $linker_version"
+echo "make version: $make_version"
+echo "util-linux version: $util_linux_version"
+echo "module-init-tools version: $module_init_tools"
+echo "ubuntu version: $ubuntu_version"
+echo "dpkg -l recorded"
 
 # Save the system info to a file
 echo "Date: $now" >> system_info.txt
@@ -33,3 +45,9 @@ echo "Bison version: $bison_version" >> system_info.txt
 echo "LLVM version: $llvm_version" >> system_info.txt
 echo "CMake version: $cmake_version" >> system_info.txt
 echo "GCC version: $gcc_version" >> system_info.txt
+echo "linker version: $linker_version" >> system_info.txt
+echo "make version: $make_version" >> system_info.txt
+echo "util-linux version: $util_linux_version" >> system_info.txt
+echo "module-init-tools version: $module_init_tools" >> system_info.txt
+echo "ubuntu version: $ubuntu_version" >> system_info.txt
+echo "packages installed: $packinst_list" >> system_info.txt
